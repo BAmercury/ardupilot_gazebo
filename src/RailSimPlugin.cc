dac9ac95e308a5291c6d680e75f01e313d26282f
Brian <ba555@drexel.edu>
2019-07-25 11:16:41 -0400

    Added test world for rail sim, added in timer feedback for wave generation

--- src/RailSimPlugin.cc
+++ src/RailSimPlugin.cc
@@ -4,6 +4,8 @@
 #include <gazebo/physics/physics.hh>
 #include <gazebo/common/common.hh>
 #include <ignition/math4/ignition/math/Vector3.hh>
+#include <stdio.h>
+#include <math.h>
 
 namespace gazebo
 {
@@ -17,14 +19,16 @@ namespace gazebo
 
             // Listen for update event
             this->updateConnection = event::Events::ConnectWorldUpdateBegin(
-                std::bind(&RailSim::OnUpdate, this));
+                std::bind(&RailSim::OnUpdate, this, std::placeholders::_1));
         }
         // Called by the world update start event
-        public: void OnUpdate()
+        public: void OnUpdate(const common::UpdateInfo &_info)
         {
             // Apply Linear Velocity to the model
+            // Velocity = -aw * sin(wt)
             static double desired_cart_vel = 0.3000;
-            gzdbg << "Cart Speed " << desired_cart_vel << std::endl;
+            gzdbg << "Simulation Time" << _info.simTime.Double() << std::endl; // Time in seconds
+            //gzdbg << "Cart Speed " << desired_cart_vel << std::endl;
             this->model->SetLinearVel(ignition::math::Vector3d(desired_cart_vel, 0, 0));
         }
 
@@ -36,6 +40,11 @@ namespace gazebo
 
         // Pointer for the update even updateConnection
         private: event::ConnectionPtr updateConnection;
+
+        // Sine Wave Parameters:
+        private: const double amplitude = 15.0;
+        private: const double max_velocity = 2.0; // ft/s
+        private: const double frequency_w = 0.133;  // maxVel/Amplitude
     };
 	// Register the plugin with the simulator
 	GZ_REGISTER_MODEL_PLUGIN(RailSim)
