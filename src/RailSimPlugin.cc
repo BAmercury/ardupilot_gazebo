2f129c9a359fc0960f11f2e61d0410765ae896a5
Brian <ba555@drexel.edu>
2019-07-26 11:18:34 -0400

    Changed some logic for pausing, feature works

--- src/RailSimPlugin.cc
+++ src/RailSimPlugin.cc
@@ -25,6 +25,11 @@ namespace gazebo
             sensors::SensorManager *manager = sensors::SensorManager::Instance();
             this->sensor =  std::dynamic_pointer_cast<sensors::ContactSensor>(manager->GetSensor(this->sensor_name));
             this->sensor->SetActive(true);
+
+            // Get World Pointer
+            this->world_ptr = this->model->GetWorld();
+
+
             /*
                 worldUpdateBegin event is fired at each physics iteration
                 
@@ -36,35 +41,41 @@ namespace gazebo
         // Called by the world update start event
         public: void OnUpdate(const common::UpdateInfo &_info)
         {
-            // Apply Linear Velocity to the model
-            // Velocity = -aw * sin(wt)
-            //double desired_vel = (-amplitude * frequency_w) * sin( (frequency_w) * _info.simTime.Double());
-            //this->sensor->Update(true);
-            // 
+
+            // Check if drone has landed on the platform
             msgs::Contacts contacts;
             contacts = this->sensor->Contacts();
             for (unsigned int i = 0; i < contacts.contact_size(); ++i)
             {
-
+                // If contact is made between platform and drone, pause the simulation
                 if (contacts.contact(i).collision1() == this->drone_coll_name)
                 {
                     gzdbg << "Collision has been detected"<< std::endl;
+                    this->model->SetLinearVel(ignition::math::Vector3d(0,0,0));
+                    this->world_ptr->SetPaused(true);
+                    contacted = true;
+                    break;
+
+                }
+                else if (contacted == false)
+                {
+                    // Apply Linear Velocity to the model
+                    // Velocity = -aw * sin(wt)physics::WorldPtr _parent, sd
+                    double desired_vel = (-amplitude * frequency_w) * sin( (frequency_w) * _info.simTime.Double());
+
+                    // Should oscillate between 0.6096:-0.6096 m/s ? 
+                    desired_vel = desired_vel * 0.2;
+                    gzdbg << "Cart Scaled Speed: " << desired_vel << std::endl;
+                    this->model->SetLinearVel(ignition::math::Vector3d(desired_vel, 0, 0));
 
                 }
 
 
             }
-            //gzdbg << "Cart Speed: " << desired_vel << std::endl; // Time in seconds
-            //gzdbg << "Cart Speed " << desired_cart_vel << std::endl;
-
-            // Should oscillate between 0.6096:-0.6096 m/s ? 
-            //desired_vel = desired_vel * 0.2;
-            //gzdbg << "Cart Scaled Speed: " << desired_vel << std::endl;
-            //this->model->SetLinearVel(ignition::math::Vector3d(desired_vel, 0, 0));
         }
 
 
-        // Private Members
+        // Private Membersphysics::WorldPtr _parent, sd
 
         // Pointer to the model
         private: physics::ModelPtr model;
@@ -81,6 +92,10 @@ namespace gazebo
         private: sensors::ContactSensorPtr sensor;
         private: const std::string sensor_name = "contact_sensor";
         private: const std::string drone_coll_name = "iris::iris_demo::iris::base_link::base_link_collision";
+        private: bool contacted = false;
+
+        // World Pointer
+        private: physics::WorldPtr world_ptr;
     };
 	// Register the plugin with the simulator
 	GZ_REGISTER_MODEL_PLUGIN(RailSim)
