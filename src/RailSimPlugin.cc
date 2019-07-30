72876bfe5db5162324323e7f4fc45f6af6b126af
Brian Amin <ba555@drexel.edu>
2019-07-30 15:45:52 -0400

    Changed axis for movement of railsim
    Also throwing up copy of the gazebo iris param file. Setting rc 8 1900 enabled precision loiter, data looks good

--- src/RailSimPlugin.cc
+++ src/RailSimPlugin.cc
@@ -66,7 +66,7 @@ namespace gazebo
                     // Should oscillate between 0.6096:-0.6096 m/s ? 
                     desired_vel = desired_vel * 0.2;
                     //gzdbg << "Cart Scaled Speed: " << desired_vel << std::endl;
-                    this->model->SetLinearVel(ignition::math::Vector3d(desired_vel, 0, 0));
+                    this->model->SetLinearVel(ignition::math::Vector3d(0, desired_vel, 0));
 
                 }
 
