9361309d2e7642683de2781d9869600fdc0fc472
Brian Amin <ba555@drexel.edu>
2019-08-15 09:22:50 -0400

    control system model, debugging gazebo 9

--- ardupilot_extra/tether_control_model/model.py
+++ ardupilot_extra/tether_control_model/model.py
@@ -2,27 +2,30 @@ import control
 import numpy as np
 from matplotlib import pyplot as plt
 
-m = 1.5
-k = 0.1
-g = 9.8
+m = 1.5 # Kg, 3DR Iris Drone
+k = 0.1 # Air resistance coefficient estimate
+g = 9.8 # M/s^2
 A = -k/m
 B = 1/m
 C = 1
 D = 0
-u = 100 # Newtons
+u = 100 # Newtons step input
+# Make a state space model
 sys = control.StateSpace(A,B,C,D)
 print(sys)
-
+# Look at poles of model
 control.pzmap(sys)
 
+# Plot step response open loop
 plt.figure(2)
 
 T, yout = control.step_response(u*sys)
 plt.plot(T, yout)
 plt.title("Open Loop Response")
 
-
+# State feedback with feedforward gain for tracking
 G = 7.4 # places poles at -5
+#G = 7.8
 #G = 1
 # Calculate Scaling Factor
 s = 1
@@ -32,16 +35,22 @@ n_x = N[0]
 n_u = N[1]
 Nbar = n_u + G*n_x
 FFG = Nbar.item(1)
-r = 10 # M/s
+print(FFG)
+r = 30 # M/s
 u = r
-u = r + g # Gravity as input bias
-sys_fdbk = control.StateSpace(A-(B*G), B*FFG, C, D)
+u = FFG*r
+# Add distrubance from gravity/other process variation
+
+w = g*m
+u = u + w
+
+sys_fdbk = control.StateSpace(A-(B*G), B, C, D)
 control.pzmap(sys_fdbk)
 plt.figure(4)
 
-T, yout = control.step_response(r*sys_fdbk)
+T, yout = control.step_response(u*sys_fdbk)
 plt.plot(T,yout)
-plt.title("Closed Loop Response, pole at -5")
+plt.title("Closed Loop Response")
 
 plt.show()
 
