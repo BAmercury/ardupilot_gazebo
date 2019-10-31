dadfdbc1ce4720d07db541add92d3fc74a7cfed9
Brian Amin <ba555@drexel.edu>
2019-10-31 13:02:28 -0400

    update

--- include/AnglePlugin.hh
+++ include/AnglePlugin.hh
@@ -5,7 +5,10 @@
 #include "gazebo/common/Event.hh"
 #include "gazebo/common/Plugin.hh"
 #include <ignition/math4/ignition/math/Vector3.hh>
-
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+using raw_type = void;
 
 namespace gazebo
 {
@@ -14,13 +17,28 @@ namespace gazebo
     {
         public: virtual void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf);
 
-        public: void OnUpdate();
+        public: void OnUpdate(const common::UpdateInfo &_info);
+
+        public: struct DataPacket
+        {
+            uint64_t timestamp;
+            uint16_t num_targets;
+            float pos_x;
+            float pos_y;
+            float size_x;
+            float size_y;
+        };
+
+        public: std::string irlock_addr = "127.0.0.1";
+        public: uint16_t irlock_port = 9005;
+
 
 
         private: physics::WorldPtr world;
         private: event::ConnectionPtr updateConnection;
         private: physics::ModelPtr model_drone;
         private: physics::ModelPtr model_target;
+
     };
 
 }
--- src/AnglePlugin.cc
+++ src/AnglePlugin.cc
@@ -7,4 +7,22 @@ using namespace gazebo;
 
 GZ_REGISTER_WORLD_PLUGIN(AnglePlugin)
 
+void AnglePlugin::Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+{
+    this->world = _world;
 
+
+
+
+
+
+
+
+    this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+        std::bind(&AnglePlugin::OnUpdate, this, std::placeholders::_1));
+}
+
+void AnglePlugin::OnUpdate(const common::UpdateInfo &_info)
+{
+
+}
