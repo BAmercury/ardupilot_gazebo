7d5d6a8559ea085b00cb0fcdea89cf132cc53c4f
Brian Amin <ba555@drexel.edu>
2019-11-15 13:28:30 -0500

    Final update to the plugin, normalized output vector

--- src/AnglePlugin.cc
+++ src/AnglePlugin.cc
@@ -117,7 +117,8 @@ void AnglePlugin::OnUpdate(const common::UpdateInfo &_info)
         this->rot = this->Rx * this->Ry * this->Rz;
         ignition::math::Vector3d corrected_pos = this->rot * this->rel_pos_NED;
 
-        corrected_pos = corrected_pos / this->drone_pose.Pos().Z();
+        //corrected_pos = corrected_pos / this->drone_pose.Pos().Z();
+        corrected_pos /= corrected_pos.Length();
         // Get angles
         //float angle_x = atan2(corrected_pos.X(),  corrected_pos.Z()); // In Radians
         //float angle_y = atan2(corrected_pos.Y(), corrected_pos.Z());
